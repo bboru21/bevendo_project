c05b31987fa751e4128210ac92f7282166ea7a9f
bboru21 <bryan.e.hadro@gmail.com>
2021-08-24 21:41:39 -0400

    adds static settings

--- bevendo/bevendo/settings/base.py
+++ bevendo/bevendo/settings/base.py
@@ -254,7 +254,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#static-root
+STATIC_ROOT = SITE_ROOT / 'assets'
+
 STATIC_URL = '/static/'
+
+# See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
+STATICFILES_DIRS = (
+    SITE_ROOT / 'static',
+)
+
+# See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
 ########## END STATIC FILE CONFIGURATION
 
 
