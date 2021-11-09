c9f148a31d40285f9a6a19f50787f1191fe622d5
Bryan Hadro <bryan.e.hadro@gmail.com>
2021-11-09 10:59:50 -0500

    adds IngredientTestCase

--- README.md
+++ README.md
@@ -6,3 +6,20 @@ Django layout is based off of the [Django Twoscoops Project].
 
 [django twoscoops project]: https://github.com/twoscoops/django-twoscoops-project/
 [drinking with the saints]: https://drinkingwiththesaints.com/
+
+# Testing
+
+Running unittests:
+
+    bevendo_project$ bevendo/manage.py test
+
+Running unitests with coverage:
+
+    bevendo_project$ coverage run --source='.' bevendo/manage.py test
+    bevendo_project$ coverage run --source='.' bevendo/manage.py test api
+    bevendo_project$ coverage report
+
+## Links
+
+    https://docs.python.org/3/library/unittest.html#unittest.TestCase
+    https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Testing
--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from api.models import Ingredient
 
-# Create your tests here.
+class IngredientTestCase(TestCase):
+    def setUp(self):
+        Ingredient.objects.create(
+            name='Cool Cool Water',
+            is_controlled=False,
+        )
+        Ingredient.objects.create(
+            name='Red Red Wine',
+            is_controlled=True,
+        )
+
+    def test_is_controlled(self):
+        water = Ingredient.objects.get(name='Cool Cool Water')
+        wine = Ingredient.objects.get(name='Red Red Wine')
+
+        self.assertFalse(water.is_controlled)
+        self.assertTrue(wine.is_controlled)
+
+    def test_str(self):
+        water = Ingredient.objects.get(name='Cool Cool Water')
+        self.assertIsInstance(water.__str__(), str)
+
+    def test_name_max_length(self):
+        ingredient = Ingredient.objects.get(pk=1)
+        max_length = ingredient._meta.get_field('name').max_length
+        self.assertEqual(max_length, 250)
