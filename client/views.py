2386af4bb4770f9fd4349f8cb49b4c6d4112df76
Bryan Hadro <bryan.e.hadro@gmail.com>
2021-11-11 21:01:17 -0500

    adds most common cocktail ingredients

--- client/views.py
+++ client/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models import Count
 
+from api.models import CocktailIngredient
 from api.utils import get_email_deals
 from ext_data.models import get_latest_price_pull_date
+
 
 def index(request):
 
     latest_pull_date = get_latest_price_pull_date()
     deals = get_email_deals(latest_pull_date)
 
+    cocktail_ingredients = CocktailIngredient.objects \
+        .filter(ingredient__is_controlled=True) \
+        .values('ingredient__name') \
+        .annotate(ingredient_count=Count('ingredient')) \
+        .order_by('-ingredient_count')
+
     context = {
+        'cocktail_ingredients': cocktail_ingredients,
         'deals': deals,
         'latest_pull_date': latest_pull_date.strftime('%B %d, %Y'),
     }
